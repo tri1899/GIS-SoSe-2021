aae2897356c75435fa2b3fdae25065b43d2049a5
tri1899 <tri1899@gmail.com>
2021-04-21 08:33:38 +0200

    Update

--- Aufgabe_5_Einführung_Script/script.js
+++ Aufgabe_5_Einführung_Script/script.js
@@ -6,4 +6,30 @@ console.log("5" + 1);
 //console.log('5' - 1);
 //BONUS:
 //console.log(('b' + 'a' + + 'a' + 'a').toLowerCase());
+let five = 5;
+console.log(five * five);
+let weather = "sunny";
+console.log(weather);
+weather = "dark";
+console.log(weather);
+let lukasDebt = 1000;
+lukasDebt = lukasDebt - 50;
+console.log(lukasDebt);
+let one = 1, two = 2;
+console.log(one + two);
+let name1 = "Lukas";
+const greeting = "Hello ";
+console.log(greeting + name1);
+//prompt("Passwort eingeben");
+let x = Math.max(2, 4);
+console.log(x);
+console.log(Math.min(2, 4) + 10);
+/*let userInput: string = prompt("Pick a number");
+let userNumber: number = Number(userInput);
+console.log("The square of your number is " + userNumber * userNumber);
+
+function add(_a: number, _b: number): number {
+    let result: number = _a + _b;
+    return result;
+}*/
 //# sourceMappingURL=script.js.map
--- Aufgabe_5_Einführung_Script/script.ts
+++ Aufgabe_5_Einführung_Script/script.ts
@@ -6,3 +6,41 @@ console.log("5" + 1);
 
 //BONUS:
 //console.log(('b' + 'a' + + 'a' + 'a').toLowerCase());
+
+
+let five: number = 5;
+console.log(five * five);
+
+let weather: string = "sunny";
+console.log(weather);
+weather = "dark";
+console.log(weather);
+
+let lukasDebt: number = 1000;
+lukasDebt = lukasDebt - 50;
+console.log(lukasDebt);
+
+let one: number = 1, two: number = 2;
+console.log(one + two);
+
+let name1: string = "Lukas";
+const greeting: string = "Hello ";
+console.log(greeting + name1);
+
+//prompt("Passwort eingeben");
+
+let x: number = Math.max(2, 4);
+console.log(x);
+
+console.log(Math.min(2, 4) + 10);
+
+/*let userInput: string = prompt("Pick a number");
+let userNumber: number = Number(userInput);
+console.log("The square of your number is " + userNumber * userNumber);
+
+function add(_a: number, _b: number): number {
+    let result: number = _a + _b;
+    return result;
+}*/
+
+
