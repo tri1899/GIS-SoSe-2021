547d6f43eea8066c5344057a447c8b7413cf58e6
tri1899 <tri1899@gmail.com>
2021-05-30 12:25:13 +0200

    Update

--- Aufgabe_10_Serveranbindung/script.js
+++ Aufgabe_10_Serveranbindung/script.js
@@ -1,26 +1,26 @@
 "use strict";
-var Aufgabe3_1;
-(function (Aufgabe3_1) {
-    let button = document.getElementById("button");
-    button.addEventListener("click", handle);
-    async function handle() {
+var P_3_1Server;
+(function (P_3_1Server) {
+    //Teilaufgabe 2
+    async function sendData() {
         let formData = new FormData(document.forms[0]);
-        let _url = "https://tri1899gissose2021.herokuapp.com/";
-        let query = new URLSearchParams(formData);
-        _url = _url + "?" + query.toString();
-        console.log(_url);
-        await fetch(_url);
-        for (let entry of query) {
+        console.log(":" + formData.get("name")); //Konsolenausgabe
+        for (let entry of formData) {
             console.log(entry);
-            console.log("name: " + entry[0]);
-            console.log("value: " + entry[1]);
+            console.log("name: " + entry[0]); ///Konsolenausgabe Eingang Stelle 0
+            console.log("name: " + entry[1]); //Konsolenausgabe
         }
-        let response = await fetch(_url);
-        let answer = await response.text();
-        console.log(answer);
-        let paragraph = document.createElement("p");
-        paragraph.innerText = answer;
-        document.body.appendChild(paragraph);
+        let query = new URLSearchParams(formData);
+        let _url = "https://tri1899gissose2021.herokuapp.com/"; //Verknüpfung mit der herokuapp
+        _url = _url + "?" + query.toString(); //Url in String umwandeln
+        console.log(_url); //Konsolenausgabe
+        let response = await fetch(_url); // auf url warten
+        let antwort = await response.text(); //Auf die antwort warten
+        console.log(antwort);
+        let rueckgabe = document.getElementById("serverausgabe"); //Paragraph und id um die Eingaben auf der HTML Site anzeigen zu lassen
+        rueckgabe.innerText = antwort;
     }
-})(Aufgabe3_1 || (Aufgabe3_1 = {}));
+    let button = document.getElementById("button");
+    button.addEventListener("click", sendData); //Button um Funktion aufzurufen
+})(P_3_1Server || (P_3_1Server = {})); //Ende namespace
 //# sourceMappingURL=script.js.map
--- Aufgabe_10_Serveranbindung/script.ts
+++ Aufgabe_10_Serveranbindung/script.ts
@@ -1,26 +1,24 @@
-namespace Aufgabe3_1 {
-    let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
-    button.addEventListener("click", handle);
-
-    async function handle(): Promise<void> {
-        let formData: FormData = new FormData(document.forms[0]);
-        let _url: RequestInfo = "https://tri1899gissose2021.herokuapp.com/"; 
-        let query: URLSearchParams = new URLSearchParams (<any>formData);
-        _url = _url + "?" + query.toString();
-        console.log(_url);
-        await fetch(_url);
-
-        for (let entry of query) {
+namespace P_3_1Server {
+    //Teilaufgabe 2
+    async function sendData(): Promise<void> { //async Funktion
+        let formData: FormData = new FormData (document.forms[0]);
+        console.log(":" + formData.get ("name")); //Konsolenausgabe
+        for (let entry of formData) {
             console.log(entry);
-            console.log("name: " + entry[0]);
-            console.log("value: " + entry[1]);
-            
+            console.log("name: " + entry[0]); ///Konsolenausgabe Eingang Stelle 0
+            console.log("name: " + entry[1]); //Konsolenausgabe
         }
-        let response: Response = await fetch(_url);
-        let answer: string = await response.text();
-        console.log(answer);
-        let paragraph: HTMLParagraphElement = document.createElement("p");
-        paragraph.innerText = answer;
-        document.body.appendChild(paragraph);
+        let  query: URLSearchParams = new URLSearchParams(<any> formData);
+        let _url: RequestInfo = "https://tri1899gissose2021.herokuapp.com/"; //Verknüpfung mit der herokuapp
+        _url = _url + "?" + query.toString(); //Url in String umwandeln
+        console.log(_url); //Konsolenausgabe
+        let response: Response = await fetch (_url); // auf url warten
+        let antwort: string = await response.text(); //Auf die antwort warten
+        console.log (antwort); 
+        let rueckgabe: HTMLParagraphElement = <HTMLParagraphElement> document.getElementById("serverausgabe"); //Paragraph und id um die Eingaben auf der HTML Site anzeigen zu lassen
+        rueckgabe.innerText = antwort; 
     }
-}
+    let button: HTMLButtonElement = <HTMLButtonElement> document.getElementById("button"); 
+    button.addEventListener("click", sendData); //Button um Funktion aufzurufen
+  
+}//Ende namespace
