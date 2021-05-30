d48b3a5e9240cada491c02f3a681e8531311287d
tri1899 <tri1899@gmail.com>
2021-05-30 13:55:12 +0200

    Update

--- Aufgabe_10_Serveranbindung/script.js
+++ Aufgabe_10_Serveranbindung/script.js
@@ -2,8 +2,8 @@
 var P_3_1;
 (function (P_3_1) {
     let button = document.getElementById("button");
-    button.addEventListener("click", sendData); //Button um Funktion aufzurufen
-    async function sendData() {
+    button.addEventListener("click", Datensenden); //Button um Funktion aufzurufen
+    async function Datensenden() {
         let formData = new FormData(document.forms[0]); //das erste Formular des Dokuments wird ausgewertet
         console.log(":" + formData.get("name")); //Eindeutiger und bekannter Name kann ich mit get() auslesen
         for (let entry of formData) {
@@ -12,12 +12,12 @@ var P_3_1;
         }
         let query = new URLSearchParams(formData); //Daten liegen vor (von dem Formular) ich kann den String nun aus einem Form Data Objekt generieren
         let _url = "https://tri1899gissose2021.herokuapp.com/";
-        _url = _url + "?" + query.toString(); //Url in String umwandeln
-        console.log(_url); //Konsolenausgabe
-        let response = await fetch(_url); // auf url warten
-        let antwort = await response.text(); //Auf die antwort warten
+        _url = _url + "?" + query.toString(); //ich wandele meine formDaten in ein String und hänge diese an die URL
+        console.log(_url);
+        let response = await fetch(_url); // URL warten und abschicken
+        let antwort = await response.text(); //auf die Antwort warten
         console.log(antwort);
-        let rueckgabe = document.getElementById("serverausgabe"); //Paragraph und id um die Eingaben auf der HTML Site anzeigen zu lassen
+        let rueckgabe = document.getElementById("serverausgabe"); //Auf meiner HTML Seite anzeigen lassen
         rueckgabe.innerText = antwort;
     }
 })(P_3_1 || (P_3_1 = {}));
--- Aufgabe_10_Serveranbindung/script.ts
+++ Aufgabe_10_Serveranbindung/script.ts
@@ -1,8 +1,8 @@
 namespace P_3_1 {
     let button: HTMLButtonElement = <HTMLButtonElement> document.getElementById("button"); 
-    button.addEventListener("click", sendData); //Button um Funktion aufzurufen
+    button.addEventListener("click", Datensenden); //Button um Funktion aufzurufen
 
-    async function sendData(): Promise<void> { 
+    async function Datensenden(): Promise<void> { 
         let formData: FormData = new FormData (document.forms[0]); //das erste Formular des Dokuments wird ausgewertet
 
         console.log(":" + formData.get ("name")); //Eindeutiger und bekannter Name kann ich mit get() auslesen
@@ -12,12 +12,12 @@ namespace P_3_1 {
         }
         let  query: URLSearchParams = new URLSearchParams(<any> formData); //Daten liegen vor (von dem Formular) ich kann den String nun aus einem Form Data Objekt generieren
         let _url: RequestInfo = "https://tri1899gissose2021.herokuapp.com/"; 
-        _url = _url + "?" + query.toString(); //Url in String umwandeln
-        console.log(_url); //Konsolenausgabe
-        let response: Response = await fetch (_url); // auf url warten
-        let antwort: string = await response.text(); //Auf die antwort warten
+        _url = _url + "?" + query.toString(); //ich wandele meine formDaten in ein String und hänge diese an die URL
+        console.log(_url); 
+        let response: Response = await fetch (_url); // URL warten und abschicken
+        let antwort: string = await response.text(); //auf die Antwort warten
         console.log (antwort); 
-        let rueckgabe: HTMLParagraphElement = <HTMLParagraphElement> document.getElementById("serverausgabe"); //Paragraph und id um die Eingaben auf der HTML Site anzeigen zu lassen
+        let rueckgabe: HTMLParagraphElement = <HTMLParagraphElement> document.getElementById("serverausgabe"); //Auf meiner HTML Seite anzeigen lassen
         rueckgabe.innerText = antwort; 
     }
 
