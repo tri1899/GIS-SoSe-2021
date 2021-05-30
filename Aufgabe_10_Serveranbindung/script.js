d433a938edfbb48c598730fc4ee978b34d57da1f
tri1899 <tri1899@gmail.com>
2021-05-30 11:45:22 +0200

    Update

--- Aufgabe_10_Serveranbindung/script.js
+++ Aufgabe_10_Serveranbindung/script.js
@@ -1,17 +1,26 @@
 "use strict";
-var Praktikumsaufgabe_P3_1;
-(function (Praktikumsaufgabe_P3_1) {
-    async function formData(_pURL) {
-        let response = await fetch(_pURL);
-        console.log("Response", response);
-        let formData = await response.formData();
-        console.log(formData.get("fname"));
-        console.log(formData.get("femail"));
-        console.log(formData.get("fbetreff"));
-        console.log(formData.get("fdatum"));
-        console.log(formData.get("ftextfeld"));
-        console.log(formData.get("fdokument"));
+var Aufgabe3_1;
+(function (Aufgabe3_1) {
+    let button = document.getElementById("button");
+    button.addEventListener("click", handle);
+    async function handle() {
+        let formData = new FormData(document.forms[0]);
+        let _url = "https://tri1899gissose2021.herokuapp.com/";
+        let query = new URLSearchParams(formData);
+        _url = _url + "?" + query.toString();
+        console.log(_url);
+        await fetch(_url);
+        for (let entry of query) {
+            console.log(entry);
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
+        }
+        let response = await fetch(_url);
+        let answer = await response.text();
+        console.log(answer);
+        let paragraph = document.createElement("p");
+        paragraph.innerText = answer;
+        document.body.appendChild(paragraph);
     }
-    formData("https://tri1899gissose2021.herokuapp.com/"); //Link App und Github
-})(Praktikumsaufgabe_P3_1 || (Praktikumsaufgabe_P3_1 = {}));
+})(Aufgabe3_1 || (Aufgabe3_1 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe_10_Serveranbindung/script.ts
+++ Aufgabe_10_Serveranbindung/script.ts
@@ -1,16 +1,26 @@
-namespace Praktikumsaufgabe_P3_1 {
+namespace Aufgabe3_1 {
+    let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
+    button.addEventListener("click", handle);
 
-    async function formData(_pURL: RequestInfo): Promise<void> {
-        let response: Response = await fetch(_pURL);
-        console.log("Response", response);
-        let formData: FormData = await response.formData();
-        console.log(formData.get("fname"));
-        console.log(formData.get("femail"));
-        console.log(formData.get("fbetreff"));
-        console.log(formData.get("fdatum"));
-        console.log(formData.get("ftextfeld"));
-        console.log(formData.get("fdokument"));
+    async function handle(): Promise<void> {
+        let formData: FormData = new FormData(document.forms[0]);
+        let _url: RequestInfo = "https://tri1899gissose2021.herokuapp.com/"; 
+        let query: URLSearchParams = new URLSearchParams (<any>formData);
+        _url = _url + "?" + query.toString();
+        console.log(_url);
+        await fetch(_url);
 
+        for (let entry of query) {
+            console.log(entry);
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
+            
+        }
+        let response: Response = await fetch(_url);
+        let answer: string = await response.text();
+        console.log(answer);
+        let paragraph: HTMLParagraphElement = document.createElement("p");
+        paragraph.innerText = answer;
+        document.body.appendChild(paragraph);
     }
-    formData("https://tri1899gissose2021.herokuapp.com/"); //Link App und Github
 }
