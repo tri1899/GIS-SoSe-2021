cd2537c8d8df923b73780adede6c686dc819e4fe
tri1899 <tri1899@gmail.com>
2021-06-05 16:41:50 +0200

    Update

--- Aufgabe_11_Server_Request/script.ts
+++ Aufgabe_11_Server_Request/script.ts
@@ -1,6 +1,15 @@
 namespace P_3_2 {
+
+    interface FormularDaten {
+        vorname: string;
+        nachname: string;
+        adresse: string;
+        nachricht: string;
+    }
     let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("buttonhtml");
     button.addEventListener("click", HTMLdatensenden); //Button um Funktion aufzurufen
+    let buttonJSON: HTMLButtonElement = <HTMLButtonElement>document.getElementById("buttonjson");
+    buttonJSON.addEventListener("click", JSONdatensenden); //Button um Funktion aufzurufen
     let rueckgabe: HTMLParagraphElement = <HTMLParagraphElement>document.getElementById("serverausgabe"); //Auf meiner HTML Seite anzeigen lassen
 
     async function HTMLdatensenden(): Promise<void> {
@@ -18,26 +27,16 @@ namespace P_3_2 {
         rueckgabe.innerText = antwort;
     }
 
-    let buttonJSON: HTMLButtonElement = <HTMLButtonElement>document.getElementById("buttonjson");
-    buttonJSON.addEventListener("click", JSONdatensenden); //Button um Funktion aufzurufen
-
-    interface FormularDaten {
-        vorname: string;
-        nachname: string;
-        adresse: string;
-        nachricht: string;
-    }
-
     async function JSONdatensenden(): Promise<void> {
         let formData: FormData = new FormData(document.forms[0]); //das erste Formular des Dokuments wird ausgewertet
 
-        let _url: RequestInfo = "https://tri1899gissose2021.herokuapp.com/json";
+        let url: RequestInfo = "https://tri1899gissose2021.herokuapp.com/json";
 
 
         let query: URLSearchParams = new URLSearchParams(<any>formData); //Daten liegen vor (von dem Formular) ich kann den String nun aus einem Form Data Objekt generieren
-        _url = _url + "?" + query.toString(); //ich wandele meine formDaten in ein String und hänge diese an die URL
+        url = url + "?" + query.toString(); //ich wandele meine formDaten in ein String und hänge diese an die URL
 
-        let response: Response = await fetch(_url); // URL warten und abschicken
+        let response: Response = await fetch(url); // URL warten und abschicken
         let antwort: FormularDaten = await response.json(); //auf die Antwort warten
         console.log(antwort);
         
