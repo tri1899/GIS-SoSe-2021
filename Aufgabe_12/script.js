20eff30b2e3c919e404e08508598267b3a54a191
tri1899 <tri1899@gmail.com>
2021-06-18 12:20:22 +0200

    Update

--- Aufgabe_12/script.js
+++ Aufgabe_12/script.js
@@ -1,8 +1,8 @@
 "use strict";
 var Aufgabe3_4;
 (function (Aufgabe3_4) {
-    let buttonVerschicken = document.getElementById("buttonabschicken");
-    buttonVerschicken.addEventListener("click", datenspeichern);
+    let buttonspeichern = document.getElementById("buttonabschicken");
+    buttonspeichern.addEventListener("click", datenspeichern);
     async function datenspeichern() {
         let formData = new FormData(document.forms[0]);
         let url = "https://tri1899gissose2021.herokuapp.com/datenspeichern";
@@ -12,18 +12,17 @@ var Aufgabe3_4;
         let ausgabe = await antwort.text();
         console.log(ausgabe);
     }
-    let buttonAusgabe = document.getElementById("buttonanzeigen");
-    buttonAusgabe.addEventListener("click", datenAnzeigen);
+    let buttonausgeben = document.getElementById("buttonanzeigen");
+    buttonausgeben.addEventListener("click", datenAnzeigen);
     let rueckgabe = document.getElementById("serverantwort"); //anheften an die Seite
-    //Funktion um Daten auf der Seite anzuzeigen
     async function datenAnzeigen() {
         let formData = new FormData(document.forms[0]);
         let url = "https://tri1899gissose2021.herokuapp.com/datenauslesen";
         let query = new URLSearchParams(formData);
-        url = url + "?" + query.toString(); //Url in String umwandeln
+        url = url + "?" + query.toString();
         let antwort = await fetch(url);
         let ausgabe = await antwort.text();
-        rueckgabe.innerHTML = ausgabe; //Ausgabe auf der HTML Seite 
+        rueckgabe.innerHTML = ausgabe;
     }
 })(Aufgabe3_4 || (Aufgabe3_4 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe_12/script.ts
+++ Aufgabe_12/script.ts
@@ -1,7 +1,7 @@
 namespace Aufgabe3_4 {
 
-    let buttonVerschicken: HTMLButtonElement = <HTMLButtonElement> document.getElementById("buttonabschicken"); 
-    buttonVerschicken.addEventListener("click", datenspeichern); 
+    let buttonspeichern: HTMLButtonElement = <HTMLButtonElement> document.getElementById("buttonabschicken"); 
+    buttonspeichern.addEventListener("click", datenspeichern); 
 
     
     async function datenspeichern(): Promise<void> { 
@@ -21,21 +21,26 @@ namespace Aufgabe3_4 {
           
     }
 
-    let buttonAusgabe: HTMLButtonElement = <HTMLButtonElement> document.getElementById("buttonanzeigen"); 
-    buttonAusgabe.addEventListener("click", datenAnzeigen);
+    let buttonausgeben: HTMLButtonElement = <HTMLButtonElement> document.getElementById("buttonanzeigen"); 
+    buttonausgeben.addEventListener("click", datenAnzeigen);
 
     let rueckgabe: HTMLDivElement = <HTMLDivElement> document.getElementById("serverantwort"); //anheften an die Seite
 
-    //Funktion um Daten auf der Seite anzuzeigen
-    async function datenAnzeigen(): Promise <void> { 
+    async function datenAnzeigen(): Promise <void> {
+
         let formData: FormData = new FormData (document.forms[0]);
+
         let url: RequestInfo = "https://tri1899gissose2021.herokuapp.com/datenauslesen"; 
 
         let  query: URLSearchParams = new URLSearchParams(<any> formData);
-        url = url + "?" + query.toString(); //Url in String umwandeln
+
+        url = url + "?" + query.toString();
+
         let antwort: Response = await fetch (url);
+
         let ausgabe: string = await antwort.text();
-        rueckgabe.innerHTML = ausgabe;  //Ausgabe auf der HTML Seite 
+
+        rueckgabe.innerHTML = ausgabe; 
     }
 }
 
