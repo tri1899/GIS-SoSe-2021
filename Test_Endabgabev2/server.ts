87d4c5ae712ebd87bac477bebb4d5908ab687174
tri1899 <tri1899@gmail.com>
2021-07-10 11:41:39 +0200

    Test

--- Test_Endabgabev2/server.ts
+++ Test_Endabgabev2/server.ts
@@ -6,7 +6,8 @@ import * as Mongo from "mongodb";
 
 export namespace Endabgabe {
 
-    let mongoUrl: string = "mongodb+srv://Testuser:passwort@clustertristan.gdas8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    let mongoUrl: string = "mongodb+srv://Testuser:passwort@gis-sose.tg637.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    
 
 
     //1. Server starten
